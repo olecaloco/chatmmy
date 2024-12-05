a2d4e17abbc737b4f6645ccead948407d5997266
olecaloco <jedmacapagal@gmail.com>
2024-12-06 04:39:00 +0800

    Updated auth context to watch for token changes as well

--- src/helpers/authContext.tsx
+++ src/helpers/authContext.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "@/firebase";
+import { auth, db } from "@/firebase";
 import { User as FirebaseUser } from "firebase/auth";
 import Icon from "@/assets/icon.svg";
-import { getUserData } from "@/lib/api";
+import { doc, onSnapshot } from "firebase/firestore";
 
 type UserData = {
     id: string;
@@ -17,9 +16,9 @@ interface User {
 }
 
 const userObj: User = { user: null, userData: null };
-export const AuthContext = React.createContext(userObj);
+export const AuthContext = createContext(userObj);
 
-export const useAuthContext = () => React.useContext<User>(AuthContext);
+export const useAuthContext = () => useContext<User>(AuthContext);
 
 const Loader = () => (
     <div className="h-dvh flex items-center justify-center flex-1">
@@ -28,37 +27,31 @@ const Loader = () => (
 )
 
 export const AuthContextProvider = ({ children }: any) => {
-    const [user, setUser] = React.useState<FirebaseUser | null>(null);
-    const [userData, setData] = React.useState<UserData | null>(null);
-    const [loading, setLoading] = React.useState(true);
+    const [user, setUser] = useState<FirebaseUser | null>(null);
+    const [userData, setData] = useState<UserData | null>(null);
+    const [loading, setLoading] = useState(true);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-
-                setUser(user);
-
-                getUserData(user.uid)
-                    .then((snapshot) => {
-                        if (snapshot.exists() && snapshot.data()) {
-                            const snapshotData = snapshot.data();
-                            setData({ id: snapshotData.id, tokens: snapshotData.tokens });
-                        }
-
-                        setUser(user);
-                    }).catch(e => {
-                        console.error(e)
-                        setUser(user);
-                    });
-
-            } else {
-                setUser(null);
-            }
+            setUser(user);
             setLoading(false);
         });
 
         return () => unsubscribe();
     }, []);
+
+    useEffect(() => {
+        if (!user) return;
+
+        const unsubscribe = onSnapshot(doc(db, "users", user.uid), snapshot => {
+            const data = snapshot.data();
+            if (!snapshot.exists() || !data) return;
+
+            setData({ id: data.id, tokens: data.tokens })
+        });
+
+        return () => unsubscribe();
+    }, [user])
 
     return (
         <AuthContext.Provider value={{ user, userData }}>
